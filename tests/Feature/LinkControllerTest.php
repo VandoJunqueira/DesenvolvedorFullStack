37954c734ff8411ce1d9fa73d1d481551ec7f2b7
VandoJunqueira <vando_junqueira@hotmail.com>
2023-08-20 10:08:19 -0400

    feat: Adiciona testes de recurso para LinkController
    Este commit inclui testes funcionais para o controlador `LinkController`,
    garantindo o correto funcionamento das operações relacionadas a links.
    
    - `test_store`: Testa a criação de um novo link através da rota /api/links.
    - `test_index`: Testa a listagem de links através da rota /api/links.
    - `test_update`: Testa a atualização de um link existente através da rota /api/links/{id}.
    - `test_show`: Testa a exibição de um link específico através da rota /api/links/{id}.
    
    Arquivos modificados:
    - tests/Feature/LinkControllerTest.php

---
+++ tests/Feature/LinkControllerTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+
+# php artisan test --filter=LinkControllerTest
+class LinkControllerTest extends TestCase
+{
+
+    # php artisan test --filter=LinkControllerTest::test_store
+    public function test_store(): void
+    {
+        $user = User::find(1);
+        $response = $this->actingAs($user);
+
+        $response = $this->post('/api/links', [
+            'url' => 'https://www.esteticaexperts.com.br',
+        ]);
+
+        $response->dump();
+
+        $response->assertStatus(200);
+    }
+
+    # php artisan test --filter=LinkControllerTest::test_index
+    public function test_index(): void
+    {
+        $user = User::find(1);
+        $response = $this->actingAs($user);
+
+        $response = $this->get('/api/links');
+
+        $response->dump();
+
+        $response->assertStatus(200);
+    }
+
+    # php artisan test --filter=LinkControllerTest::test_update
+    public function test_update(): void
+    {
+        $user = User::find(1);
+        $response = $this->actingAs($user);
+
+        $response = $this->put('/api/links/2', [
+            'url' => 'https://www.esteticaexperts.com.br',
+            'slug' => 'rase45322',
+            'title' => 'TESTE EDITADO'
+        ]);
+
+        $response->dump();
+
+        $response->assertStatus(200);
+    }
+
+
+    # php artisan test --filter=LinkControllerTest::test_show
+    public function test_show(): void
+    {
+        $user = User::find(1);
+        $response = $this->actingAs($user);
+
+        $response = $this->get('/api/links/2');
+
+        $response->dump();
+
+        $response->assertStatus(200);
+    }
+}
