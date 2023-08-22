09003d95dedf0bde1b9281bd8773fa5faa61f64e
VandoJunqueira <vando_junqueira@hotmail.com>
2023-08-22 14:53:07 -0400

    feat: Componente DataProvider para Busca de Dados
    Neste commit, foi adicionado o componente DataProvider, que permite buscar dados de uma URL fornecida e disponibilizá-los para os componentes filhos através de slots.
    
    Arquivos Alterados:
    
    - Criado o componente DataProvider.vue para gerenciar a busca e compartilhamento de dados.
    - O componente recebe uma prop "url" para especificar a URL da qual os dados serão buscados.
    - Utilizado um slot para fornecer os dados buscados aos componentes filhos.
    - Implementada a lógica para buscar dados usando this.$http.get e atualizar o estado "data".
    
    Esse componente aumenta a reutilização e a modularidade do código, permitindo que componentes filhos se concentrem em exibir os dados recebidos sem se preocupar com a lógica de busca.

---
+++ resources/js/components/DataProvider.vue
@@ -0,0 +1,20 @@
+<template>
+    <slot :data="data">Loading...</slot>
+</template>
+
+<script>
+export default {
+    props: ["url"],
+    data() {
+        return {
+            data: {},
+        };
+    },
+    mounted() {
+        // this.$store.state.themeLayout.loading = false;
+        this.$http.get(this.url).then((response) => {
+            this.data = response.data;
+        });
+    },
+};
+</script>
