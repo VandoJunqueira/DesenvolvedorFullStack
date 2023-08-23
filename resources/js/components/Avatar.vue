a9d63e09c129fdf076b8793735b4f9cab460da33
VandoJunqueira <vando_junqueira@hotmail.com>
2023-08-23 16:36:45 -0400

    fix: Correção no componente Avatar.vue
    Este commit aborda uma correção específica no componente Avatar.vue. Agora, quando uma imagem não puder ser carregada, uma imagem de placeholder será exibida em seu lugar. Essa correção melhora a experiência do usuário, garantindo que mesmo quando uma imagem não estiver disponível, o componente ainda apresentará uma representação visual adequada.

--- resources/js/components/Avatar.vue
+++ resources/js/components/Avatar.vue
@@ -1,18 +1,38 @@
 <template>
     <div :class="['avatar', size]">
-        <img :src="src" alt="">
+        <img :src="img_src" alt="" :onerror="imgError">
     </div>
 </template>
 
 
 <script>
+import noAvatar from '@/img/no-avatar.png';
+import imgError from '@/img/no-img.png';
 
 export default {
     name: 'Avatar',
+    data() {
+        return {
+            img_src: ''
+        }
+    },
     props: {
-        src: '',
+        src: null,
         size: ''
-    }
+    },
+    methods: {
+        imgError() {
+            this.img_src = imgError;
+        }
+    },
+    watch: {
+        src() {
+            this.img_src = this.src || noAvatar;
+        }
+    },
+    mounted() {
+        this.img_src = this.src || noAvatar;
+    },
 }
 </script>
 
